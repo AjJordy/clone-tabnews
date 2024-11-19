4ef797fd7100f91c0e17848a91ac25db0306a9f6
Jordy Araújo <jordyfaria0@gmail.com>
2024-11-19 11:10:50 +0000

    /status return  updated_at

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,8 @@
 test("GET /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
