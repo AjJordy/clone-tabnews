f12a245315475ec401665064306dee72cb08a026
Jordy Araújo <jordyfaria0@gmail.com>
2024-11-27 11:58:59 +0000

    feat: add  and refatoring migrations test

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,30 +1,30 @@
-import database from "infra/database.js";
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const defaultMigrationOption = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationOption);
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOption,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    return response.status(200).json(migrations);
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
