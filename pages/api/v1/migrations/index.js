571af952ed38d2cf20a7825c9f65c03d08100bd1
Jordy Araújo <jordyfaria0@gmail.com>
2024-11-28 11:42:23 +0000

    adds  endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
   const defaultMigrationOption = {
+    dbClient: dbClient,
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"),
@@ -13,6 +17,7 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOption);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -21,6 +26,7 @@ export default async function migrations(request, response) {
       ...defaultMigrationOption,
       dryRun: false,
     });
+    await dbClient.end();
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
